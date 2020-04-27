13c25b4444d3566d1899c3910919c829ddb062e2
Linear N.Y. Li <nli@mozilla.com>
2020-04-27 16:39:39 +0800

    pre-tagging script - to obtain commit hash value and branch

---
+++ pre-tagging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+#
+# To get GIT_CLONE_COMMIT_HASH and Branch 
+# usage: ./pre-tagging.sh build_number
+# 
+
+build_number=$1
+
+build_slug=$(curl -X GET "https://api.bitrise.io/v0.1/apps/$app_slug/builds?sort_by=created_at&build_number=$1" -H  "accept: application/json" -H  "Authorization: $BITRISE_TOKEN" | jq -r '.data[].slug')
+
+curl -X GET "https://api.bitrise.io/v0.1/apps/$app_slug/builds/$build_slug/log" -H  "accept: application/json" -H  "Authorization: $BITRISE_TOKEN" | jq '.expiring_raw_log_url' | xargs wget -O build.log 
+
+commit_hash=$(cat build.log | grep -A1 HASH | grep value | awk '{print $2}')
+branch=$(cat build.log | grep -w "Branch:"  | awk '{print $3}')
+
+echo "Commit_Hash: "$commit_hash
+echo "Branch: "$branch
+
+rm build.log
+
