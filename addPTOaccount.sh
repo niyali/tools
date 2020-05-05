f94acb3bc2eed777eb06824900dc0f6044658485
Linear N.Y. Li <nli@mozilla.com>
2020-05-05 11:36:40 +0800

    add script usage and minor change

--- addPTOaccount.sh
+++ addPTOaccount.sh
@@ -1,14 +1,20 @@
 #!/bin/bash/
 
+# prerequisite:
+# - install mysql-shell, https://dev.mysql.com/downloads/shell/
+# - need to be at corp network 
+# 
+# Usage: addPTOaccount.sh Employee_Number Name Email Manager PTO 
+
 Employee_Number=$1
 Name=$2
 Email=$3
 Manager=$4
-Status=$5
-StartDate=$6
-PTO=$7
+Status=1
+StartDate=`date +%Y-%m-%d`
+PTO=$5
 
 source addPTOaccount_config.sh
 `ssh -i ~/.ssh/id_rsa_pc $username@$hostaddr -L 3306:127.0.0.1:3306 -N &`
-mysqlsh --host=127.0.0.1 --sql --port=3306 -u root -p -e "use $database; INSERT INTO $table ($T_field1, $T_field2, $T_field3, $T_field4, $T_field5, $T_field6, $T_field7, $T_field8) VALUES ('$1', '$2', '$3', '$4', '$5', '$6', '$7', '$7');"
+#mysqlsh --host=127.0.0.1 --sql --port=3306 -u root -p -e "use $database; INSERT INTO $table ($T_field1, $T_field2, $T_field3, $T_field4, $T_field5, $T_field6, $T_field7, $T_field8) VALUES ('$1', '$2', '$3', '$4', '$Status', '$StartDate', '$5', '$5');"
 
