8d58393bbeca10d804a58fe769f37d43ba121c1e
Linear N.Y. Li <nli@mozilla.com>
2020-06-03 16:17:36 +0800

    reverted comments

--- onborading.sh
+++ onborading.sh
@@ -15,8 +15,8 @@ StartDate=`date +%Y-%m-%d`
 PTO=$5
 
 source onborading_config.sh
-#`ssh -i ~/.ssh/id_rsa_pc $username@$hostaddr -L 3306:127.0.0.1:3306 -N &`
-#mysqlsh --host=127.0.0.1 --sql --port=3306 -u root -p -e "use $database; INSERT INTO $table ($T_field1, $T_field2, $T_field3, $T_field4, $T_field5, $T_field6, $T_field7, $T_field8) VALUES ('$1', '$2', '$3', '$4', '$Status', '$StartDate', '$5', '$5');"
+`ssh -i ~/.ssh/id_rsa_pc $username@$hostaddr -L 3306:127.0.0.1:3306 -N &`
+mysqlsh --host=127.0.0.1 --sql --port=3306 -u root -p -e "use $database; INSERT INTO $table ($T_field1, $T_field2, $T_field3, $T_field4, $T_field5, $T_field6, $T_field7, $T_field8) VALUES ('$1', '$2', '$3', '$4', '$Status', '$StartDate', '$5', '$5');"
 
 ## update Taipei employee email group if needed 
 check_manager=($(gam info group $group_email  | grep member | grep "\-all"  | awk '{print $2}'| sed 's/-all//g'))
