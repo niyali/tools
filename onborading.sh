5e1eccf6134571a698f4f234c1a9949aae3f8efe
Linear N.Y. Li <nli@mozilla.com>
2020-06-03 14:44:51 +0800

    1. To add newhire to office email group 2. Rename the script to match what it does now.

--- onborading.sh
+++ onborading.sh
@@ -14,7 +14,11 @@ Status=1
 StartDate=`date +%Y-%m-%d`
 PTO=$5
 
-source addPTOaccount_config.sh
+source onborading_config.sh
 `ssh -i ~/.ssh/id_rsa_pc $username@$hostaddr -L 3306:127.0.0.1:3306 -N &`
 mysqlsh --host=127.0.0.1 --sql --port=3306 -u root -p -e "use $database; INSERT INTO $table ($T_field1, $T_field2, $T_field3, $T_field4, $T_field5, $T_field6, $T_field7, $T_field8) VALUES ('$1', '$2', '$3', '$4', '$Status', '$StartDate', '$5', '$5');"
 
+## update Taipei employee email group if needed 
+if [[ ! " ${check_manager[@]} " =~ " $4 " ]]; then 
+    `gam update group $group_email add member $3` 
+fi
