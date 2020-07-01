f0b3e62b36eba61d74eb4ce2113c59a072535fb5
Linear N.Y. Li <nli@mozilla.com>
2020-07-01 15:03:40 +0800

    rename

--- onboarding.sh
+++ onboarding.sh
@@ -29,7 +29,7 @@ if [ $# -ne 5 ]; then
     usage;
 fi
 
-source onborading_config.sh
+source onboarding_config.sh
 `ssh -i ~/.ssh/id_rsa_pc $username@$hostaddr -L 3306:127.0.0.1:3306 -N &`
 mysqlsh --host=127.0.0.1 --sql --port=3306 -u root -p -e "use $database; INSERT INTO $table ($T_field1, $T_field2, $T_field3, $T_field4, $T_field5, $T_field6, $T_field7, $T_field8) VALUES ('$1', '$2', '$3', '$4', '$Status', '$StartDate', '$5', '$5');"
 
